5e690e36f012bc09ef2bb2c28f82051bd6569899
Neha Padala <58177161+spadala13@users.noreply.github.com>
2020-11-20 10:57:31 -0500

    poswords

--- positive-words.py
+++ positive-words.py
@@ -3,3 +3,13 @@ import json
 filename = "positive-words.txt"
 
 lofwords = []
+
+with open(filename) as pw:
+    for line in pw:
+        pword = line.strip().split(None)
+        
+        lofwords.append(pword)
+
+out_file = open("positive-words.json", "w")
+json.dump(lofwords, out_file, sort_keys=False)
+out_file.close
